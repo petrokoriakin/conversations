e824b3495ae90a4922955fbbf599bdcbedb6d36d
Pyotr K <pyotr.koryakin@svitla.com>
2011-06-01 02:19:09 +0300

    delete dummy code

--- app/models/conversation.rb
+++
@@ -1,7 +0,0 @@
-class Conversation < ActiveRecord::Base
-attr_accessible :content, :order, :done
-
-  def to_json(options = {})
-    super(options.merge(:only => [ :id, :content, :order, :done ]))
-  end
-end
